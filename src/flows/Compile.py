63b711426b9cc0f2567cd708ecb84dc297dd7993
Daniel <136742291+danlima133@users.noreply.github.com>
2024-06-21 15:30:30 +0000

    feat: organize class

--- src/flows/Compile.py
+++ src/flows/Compile.py
@@ -1,4 +1,5 @@
 import config
+import data as code
 from parse import InterfaceParse
 
 class Compile(InterfaceParse):
@@ -10,9 +11,12 @@ class Compile(InterfaceParse):
             if self.args.token == config.data["tokens"]["member"]:
                 if self.args.autocompile == True:
                     print("compile member (auto)")
+                    return code.FLOW_PASSED
                 else:
                     if len(self.args.data) == 2:
                         print("compile member in " + self.args.data[0] + " to " + self.args.data[1])
+                        return code.FLOW_PASSED
                     else:
-                        self.parse.error("paths not founds or not passeds")  
+                        self.parse.error("paths not founds or not passeds")
+                        code.FLOW_DATA_INVALID 
                         
--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@ import argparse
 import config
 from parse import Parse
 
-from Init import *
-from Compile import *
+import flows.Compile as compile
+import flows.Init as init
 
 parse:argparse.ArgumentParser
 
@@ -16,17 +16,17 @@ def main():
     parse.add_argument("token", nargs="?", default=config.data["tokens"]["default"], choices=config.data["tokens"].values(), help=config.data["helpers"]["tokens"])
     parse.add_argument("data", nargs="*", default="", help=config.data["helpers"]["data"])
 
-    parse.add_argument(config.data["flags"]["autocompile"], config.data["flags-abbrv"]["autocompile"], action="store_true", help=config.data["flagsHelpers"]["flagcompile"])
+    parse.add_argument(config.formatToFlag(config.data["flags"]["autocompile"], False), config.formatToFlag(config.data["flags-abbrv"]["autocompile"], True), action="store_true", help=config.data["flagsHelpers"]["flagcompile"])
 
     args = parse.parse_args()
     parseArgs(args)
 
 def parseArgs(args):
     global parse
-    p = Init(args, parse)
+    p = init.Init(args, parse)
     Parse(p)
 
-    p2 = Compile(args, parse)
+    p2 = compile.Compile(args, parse)
     Parse(p2)
 
 if __name__ == "__main__":
