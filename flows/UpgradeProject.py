c50f49685bd56a77aff37aa7d675cc88b1d05342
Daniel <136742291+danlima133@users.noreply.github.com>
2024-06-28 02:39:53 +0000

    feat: add module 'console' with lib rich

--- flows/UpgradeProject.py
+++ flows/UpgradeProject.py
@@ -1,5 +1,7 @@
 import re
+
 from interfaces.InterfaceFlow import InterfaceFlow
+from modules.console import console
 
 REGEX_VERSION_SEMANTIC = "v\d+.\d+.\d+"
 
@@ -9,6 +11,13 @@ class UpgradeProject(InterfaceFlow):
             version = metadata.data[0]
             isValide = re.findall(REGEX_VERSION_SEMANTIC, version)
             if len(isValide) == 1:
+                console().print(f":white_check_mark: Upgrade to [green]{version}[/green] with succssefuly")
                 return "err_ok"
-            else: return "err_data_invalid"
+            else: 
+                console().print(f":warning: SemVer invalid [red]{version}[/red]")
+                return "err_text_format"
+        console().print(":thumbs_up: Upgrade to latest version with succssefuly")
         return "err_ok"
+    
+    def __str__(self):
+        return "Upgrade CLI"
